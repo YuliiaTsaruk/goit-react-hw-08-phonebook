fd2d15142c380144f54ecef9bd1a4bee964def24
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-02-10 16:23:54 +0100

    add autorization operation and slice

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -3,11 +3,40 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://goit-task-manager.herokuapp.com/';
 
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 const clearAuthHeader = () => {
   axios.defaults.headers.common.Authorization = '';
 };
 
-export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+export const register = createAsyncThunk(
+  'authorization/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/signup', credentials);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  'authorization/logIn',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/login', credentials);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logOut', async (_, thunkAPI) => {
   try {
     const response = await axios.post('/users/logout');
     clearAuthHeader();
@@ -16,3 +45,23 @@ export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
     return thunkAPI.rejectWithValue(error.message);
   }
 });
+
+export const refresh = createAsyncThunk(
+  'authorization/refresh',
+  async (_, thunkAPI) => {
+    const { token } = thunkAPI.getState().authorization;
+
+    if (!token) {
+      return thunkAPI.rejectWithValue('No valid token');
+    }
+
+    setAuthHeader(token);
+
+    try {
+      const response = await axios.get('/users/current');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,19 +1,63 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { logIn, logOut, refresh, register } from './authOperations';
 
 export const authSlice = createSlice({
   name: 'authorization',
   initialState: {
     user: { name: null, email: null },
-    isLoggedin: false,
+    isLoggedIn: false,
     token: '',
     isRefreshing: false,
     isError: false,
   },
 
-  reducers: {
-    resetError: state => {
-      state.isError = false;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isError = false;
+      })
+      .addCase(register.rejected, state => {
+        state.isLoggedIn = false;
+        state.isError = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.isError = false;
+      })
+      .addCase(logIn.rejected, state => {
+        state.isLoggedIn = false;
+        state.isError = true;
+      })
+      .addCase(logOut.fulfilled, (state, action) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+        state.isError = false;
+      })
+      .addCase(logOut.rejected, state => {
+        state.isLoggedIn = true;
+        state.isError = true;
+      })
+
+      .addCase(refresh.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(refresh.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isRefreshing = false;
+        state.isLoggedIn = true;
+        state.isError = false;
+      })
+      .addCase(refresh.rejected, state => {
+        state.isRefreshing = false;
+        state.isLoggedIn = false;
+        state.isError = false;
+      });
   },
 });
 
